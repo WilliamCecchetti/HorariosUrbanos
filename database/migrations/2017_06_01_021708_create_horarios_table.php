e53517714b5e50773051d7f61b470084c3d1372e
williamCecchetti <williamprojetos@outlook.com.br>
2017-06-01 00:36:42 -0300

    meu primeiro commit

---
+++ database/migrations/2017_06_01_021708_create_horarios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHorariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_hora'); 
+            $table->integer('id_rota'); 
+            $table->integer('id_dia'); 
+            $table->integer('hora1'); 
+            $table->integer('hora2'); 
+            $table->integer('hora3'); 
+            $table->integer('hora4'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horarios');
+    }
+}
